bebeea20991aadadf7ad921a5614a51487d8d946
qinweijunlin <qinweijunlin@qq.com>
2019-04-12 18:51:00 +0800

    发布

--- bin/shouji.sh
+++ bin/shouji.sh
@@ -4,7 +4,6 @@
 set -euo pipefail
 
 #### 常量 ####
-readonly E_UNRECOGNIZED_OPTION=2
 readonly E_NOT_FOUND=127
 
 #### 包含 ####
@@ -16,13 +15,6 @@ err() {
 
 #### 变量 ####
 readonly oneself="$0"
-readonly projectnum="$1"
-
-phonenums="$(echo "$@" | sed 's/^[0-9]\{1,\}[ \t]\{1,\}//g')"
-
-set +u
-phonenum="$2"
-set -u
 
 #### 主体 ####
 if ! type phone.sh; then
@@ -30,30 +22,9 @@ if ! type phone.sh; then
   exit "${E_NOT_FOUND}"
 fi
 
-if [[ -z "${phonenum}" ]]; then
-  phonenum='all'
-fi
 
-readonly phonenum
 
-case "${projectnum}" in
-  1)
-    :
-    ;;
-  2)
-    phone.sh "${phonenum}" reboot,sleep:60,unlock
-    ;;
-  3)
-    phone.sh "${phonenum}" start-fw,sleep:15,use-fw,sleep:10,run-fw
-    ;;
-  4)
-    phone.sh "${phonenum}" start-tl,sleep:30,update-tl,sleep:15,skip-tl
-    ;;
-  5)
-    phone.sh "${phonenum}" exit-tl,exit-fw
-    ;;
-  *)
-    err "${oneself##*/}: unrecognized option '${projectnum}'"
-    exit "${E_UNRECOGNIZED_OPTION}"
-    ;;
-esac
+
+
+
+
