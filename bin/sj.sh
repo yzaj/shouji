41f52b1ea0ece4c1bc0d4e4b7034f5139c199de5
qinweijunlin <qinweijunlin@qq.com>
2019-04-14 00:19:53 +0800

    发布

--- bin/sj.sh
+++ bin/sj.sh
@@ -49,6 +49,6 @@ if [[ "${num}" == "${READMAX}" ]]; then
   exit "${E_OPERATION_FAILED}"
 fi
 
-color::countdown 20 35 '程序将在 ' 34 ' 秒后自动执行, 请停止操作' 34
+color::countdown 5 35 '程序将在 ' 34 ' 秒后自动执行, 请停止操作' 34
 
 color::timer 34 '成功: ' 32
